05f36ce89afdbb175b5e722c3b24b5963720ecde
Hanim <mousshanim12@gmail.com>
2025-11-04 18:55:15 +0100

    Add Home controller and update main app entry
    Introduced a new Home widget in controllers/home.dart and updated main.dart to use Home as the app's entry point. Removed boilerplate code and switched the theme seed color to blue, also enabled dark theme and disabled the debug banner.

---
+++ lib/controllers/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
